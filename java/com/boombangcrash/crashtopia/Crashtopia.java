722b00e6dd1bded2d8d668ba835986f55d1185ce
boombangcrash <yourboombangcrash@gmail.com>
2016-10-07 15:29:22 -0700

    Fleshed out proxies.

--- java/com/boombangcrash/crashtopia/Crashtopia.java
+++ java/com/boombangcrash/crashtopia/Crashtopia.java
@@ -1,20 +1,42 @@
 package com.boombangcrash.crashtopia	;
 
-import net.minecraft.init.Blocks;
+import com.boombangcrash.crashtopia.proxy.CommonProxy;
+
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Crashtopia.MODID, version = Crashtopia.VERSION)
-public class Crashtopia
-{
-    public static final String MODID = "crashtopia";
-    public static final String VERSION = "0.0.1";
+@Mod(modid = Reference.MODID,
+name = Reference.NAME,
+version = Reference.VERSION)
+
+public class Crashtopia {
+	
+	@Instance(Reference.MODID)
+	public static Crashtopia instance;
+	
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static CommonProxy proxy;
+    
+    @EventHandler
+    public void init(FMLPreInitializationEvent event)
+    {
+    	proxy.preInit(event);
+    }
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+        proxy.init(event);
+    }
+    
+    @EventHandler
+    public void init(FMLPostInitializationEvent event)
+    {
+    	proxy.postInit(event);
     }
 }
---
+++ java/com/boombangcrash/crashtopia/Reference.java
@@ -0,0 +1,11 @@
+package com.boombangcrash.crashtopia;
+
+public class Reference {
+	
+	public static final String MODID = "crashtopia";
+	public static final String NAME = "crashtopia";
+    public static final String VERSION = "0.0.1";
+    
+    public static final String CLIENT_PROXY_CLASS = "com.boombangcrash.crashtopia.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.boombangcrash.crashtopia.proxy.ServerProxy";
+}
